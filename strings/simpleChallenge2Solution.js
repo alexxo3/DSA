b2187cacdda288fb0b1e5f4fb485bb2165669e9e
Alexx03 <alelgneshetie@gmail.com>
2025-06-14 00:03:33 +0300

    second challenge

---
+++ strings/simpleChallenge2Solution.js
@@ -0,0 +1,13 @@
+let message = "Hello World";
+
+// Print the first character
+console.log(message[0]);
+
+//  Convert the whole string to lowercase
+console.log(message.toLowerCase());
+
+// Replace "World" with "JavaScript"
+console.log(message.replace("World", "JavaScript"));
+
+// Print the length of the final message
+console.log(message.length);
