0ab9bcf2143cd566bea249369667c2278344bf0b
Alexx03 <alelgneshetie@gmail.com>
2025-06-13 09:09:13 +0300

    challenge 1 started

---
+++ array/simpleChallenge1Solution.js
@@ -0,0 +1,13 @@
+let names = ["Alice", "Bob", "Charlie", "David"];
+
+//Print the second name ("Bob")
+print(names[1])
+
+//Add "Eve" to the end
+names.push("Eve")
+
+//Remove "Charlie"
+names.splice(2,1)
+
+//Print the final list
+print(names)
---
+++ array/simplechallenge1.md
@@ -0,0 +1,8 @@
+Given this array:
+let names = ["Alice", "Bob", "Charlie", "David"];
+
+Tasks:
+1. Print the second name ("Bob")
+2. Add "Eve" to the end
+3. Remove "Charlie"
+4. Print the final list
