dec3f9592d37f76bd13b03b5a1634f0dd035118a
Alexx03 <alelgneshetie@gmail.com>
2025-06-14 08:39:06 +0300

    third challenge

---
+++ stack/simpleChallenge3Solution.js
@@ -0,0 +1,12 @@
+let bookStack = [];
+
+// Add "Book1", "Book2", "Book3"
+bookStack.push("Book1");
+bookStack.push("Book2");
+bookStack.push("Book3");
+
+// Remove the top book
+bookStack.pop()
+
+// Print the current top book
+console.log(bookStack[bookStack.length -1])
---
+++ stack/simplechallenge3.md
@@ -0,0 +1,5 @@
+let bookStack = [];
+
+// Task 1: Add "Book1", "Book2", "Book3"
+// Task 2: Remove the top book
+// Task 3: Print the current top book
